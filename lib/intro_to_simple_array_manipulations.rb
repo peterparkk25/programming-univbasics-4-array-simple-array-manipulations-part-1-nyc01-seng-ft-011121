140611d1bc3ea5bad66e24455f775aaab5387e37
Peter Park 2.0 <peterparkk25+@gmail.com>
2020-12-31 21:19:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,6 +11,7 @@ end
 def using_pop(array)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania",
   "Antarctica"]
+  continents.pop
 end
 
 def pop_with_args(array)
