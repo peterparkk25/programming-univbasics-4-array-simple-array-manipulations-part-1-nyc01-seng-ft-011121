0c23e4257e376f5758665c35545bb595d1caf7c5
Peter Park 2.0 <peterparkk25+@gmail.com>
2020-12-31 21:19:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,7 +9,8 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania",
+  "Antarctica"]
 end
 
 def pop_with_args(array)
